b90fcb1587b56eacdfef8cfffcfe83d41b185f6f
gonzalolater <gonzalolater>
2021-06-26 12:24:07 -0300

    configuracion del webpack, instalancion de paquetes y agrego imagenes y fuente.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, {Fragment} from 'react'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+
+
+
+const App = () => {
+    return ( 
+        <Fragment>
+           <h1> React app</h1>
+            
+        </Fragment>
+     );
 }
-
+ 
 export default App;
